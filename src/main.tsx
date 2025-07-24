663556aacbc4db2b5fd1a052d6d7de6411337a87
Carl Wirkus <carlwirkus2@gmail.com>
2025-07-24 13:52:08 +1000

    issue with 117.1

---
+++ src/main.tsx
@@ -0,0 +1,65 @@
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  Outlet,
+  RouterProvider,
+  createRootRoute,
+  createRouter, createRoute,
+} from '@tanstack/react-router';
+import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
+import './styles.css';
+
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const rootRoute = createRootRoute({
+  pendingComponent: () => <div className="bg-red-500 h-screen">pending..</div>, //This pending component is never hit
+  component: () => <>
+    <Outlet />
+    <TanStackRouterDevtools />
+  </>,
+});
+
+const indexRoute = createRoute({
+  component: () => (
+      <div className="bg-green-500 h-screen">
+        loaded...
+      </div>
+  ),
+  path: "/",
+  getParentRoute: () => rootRoute,
+  loader: async ({context}) => {
+    console.log(context);
+    await sleep(2000);
+  },
+})
+
+const routeTree = rootRoute.addChildren([indexRoute]);
+
+const router = createRouter({
+  routeTree,
+  defaultPreload: 'intent',
+  scrollRestoration: true,
+  context: {
+    foo: {}
+  }
+});
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const rootElement = document.getElementById('app')!;
+const root = ReactDOM.createRoot(rootElement);
+
+
+root.render(
+  <StrictMode>
+    <div className="bg-red-500 h-screen">
+      <RouterProvider router={router} defaultPendingComponent={() => <div className="bg-yellow-500 h-screen">loading..</div>}  />
+    </div>
+  </StrictMode>
+);
